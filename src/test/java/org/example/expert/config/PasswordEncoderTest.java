4e8a1f57285665eebad2806c813df8f8b9bab255
Kim Hani <32191261@dankook.ac.kr>
2025-02-25 17:45:05 +0900

    레벨 3-1 : 1. 테스트 코드 연습 - 1 (예상대로 성공하는지에 대한 케이스입니다.)

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -20,7 +20,8 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // 인자의 순서가 잘못되어있었다.
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
